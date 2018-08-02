bbf1e8537680529ad861dd1cba4e02d78ddbf500
Michael Vorburger ⛑️ <mike@vorburger.ch>
2018-08-02 12:12:46 +0200

    add link to https://github.com/zeroturnaround/zt-exec/pull/25 in JavaDoc

--- src/main/java/ch/vorburger/exec/package-info.java
+++ src/main/java/ch/vorburger/exec/package-info.java
@@ -21,7 +21,8 @@
  * Improvements over Apache Commons Exec.
  * Proposed as contribution to Apache Commons Exec in <a href="https://issues.apache.org/jira/browse/EXEC-88">EXEC-88</a>.
  * 
- * <p>The <a href="https://github.com/zeroturnaround/zt-exec">zt-exec</a> library is something similar.</p>
+ * <p>The <a href="https://github.com/zeroturnaround/zt-exec">zt-exec</a> library is something similar.
+ * (<a href="https://github.com/zeroturnaround/zt-exec/pull/25">They unfortunately refused to back link this project</a>.)</p>
  * 
  * @see ch.vorburger.exec.ManagedProcessBuilder 
  * @see ch.vorburger.exec.ManagedProcess
