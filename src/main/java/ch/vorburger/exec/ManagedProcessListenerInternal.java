a6fd61056c3326f6325d9955cc1f8ff1071874f3
Michael Vorburger <mike@vorburger.ch>
2018-08-20 19:02:01 +0200

    add license-maven-plugin & missing headers
    Also added missing @Override to ManagedProcessListenerInternal.

--- src/main/java/ch/vorburger/exec/ManagedProcessListenerInternal.java
+++ src/main/java/ch/vorburger/exec/ManagedProcessListenerInternal.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * MariaDB4j
+ * %%
+ * Copyright (C) 2012 - 2014 Michael Vorburger
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package ch.vorburger.exec;
 
 import org.slf4j.Logger;
@@ -12,11 +31,13 @@ class ManagedProcessListenerInternal implements ManagedProcessListener {
 
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-    public void onProcessComplete(int exitValue) {
+    @Override
+	public void onProcessComplete(int exitValue) {
         logger.trace("Process completed exitValue: {}", exitValue);
     }
 
-    public void onProcessFailed(int exitValue, Throwable throwable) {
+    @Override
+	public void onProcessFailed(int exitValue, Throwable throwable) {
         logger.trace("Process failed exitValue: {}", exitValue, throwable);
     }
 }
