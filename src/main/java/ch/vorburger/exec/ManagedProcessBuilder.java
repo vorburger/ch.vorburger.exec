vorburger/ch.vorburger.exec

commit 1b71a28d41b3d7df6864d44c36b6aa85cfa2d765

Fixed quoted escaped argument if key-value pair contained spaces

src/main/java/ch/vorburger/exec/ManagedProcessBuilder.java
@@ -136,13 +136,31 @@ public class ManagedProcessBuilder {
      */
     protected ManagedProcessBuilder addArgument(String argPart1, String separator, String argPart2) {
         // @see MariaDB4j Issue #30 why 'quoting' (https://github.com/vorburger/MariaDB4j/issues/30)
-        StringBuilder sb = new StringBuilder();
-        sb.append(StringUtils.quoteArgument(argPart1));
-        sb.append(separator);
-        sb.append(StringUtils.quoteArgument(argPart2));
+        final StringBuilder sb = new StringBuilder();
+        final String arg;
+        
+        // @see MariaDB4j Issue #501 Fix for spaces in data path doesn't work on windows
+        // https://github.com/vorburger/MariaDB4j/issues/501
+        // Internally Runtime.exec is being used, which says that an argument such
+        // as --name="escaped value" isn't escaped, since there's no leading quote
+        // and it contains a space, so it reescapes it, causing applications to split
+        // it into multiple pieces, which is why we quote the whole arg (key and value) instead.
+        if("=".equals(separator)) {            
+            sb.append(argPart1);
+            sb.append(separator);
+            sb.append(argPart2);
+            arg =StringUtils.quoteArgument(sb.toString());
+        } else {
+            sb.append(StringUtils.quoteArgument(argPart1));
+            sb.append(separator);
+            sb.append(StringUtils.quoteArgument(argPart2));
+            arg = sb.toString();
+        }
+        
         // @see https://issues.apache.org/jira/browse/EXEC-93 why we have to use 'false' here
         // TODO Remove the false when commons-exec has a release including EXEC-93 fixed.
-        addArgument(sb.toString(), false);
+        addArgument(arg, false);
+        
         return this;
     }
 

commit 4381cf4d3726ddbf60f5ae925d0f7d74dbdcc8ae

Fix broken support for arguments with spaces on Linux
It was broken in 1b71a28d41b3d7df6864d44c36b6aa85cfa2d765.

See https://github.com/vorburger/ch.vorburger.exec/pull/58
for https://github.com/vorburger/MariaDB4j/issues/501.

src/main/java/ch/vorburger/exec/ManagedProcessBuilder.java
@@ -138,13 +138,13 @@ public class ManagedProcessBuilder {
         final StringBuilder sb = new StringBuilder();
         final String arg;
 
-        // @see MariaDB4j Issue #501 Fix for spaces in data path doesn't work on windows
-        // https://github.com/vorburger/MariaDB4j/issues/501
-        // Internally Runtime.exec is being used, which says that an argument such
+        // @see https://github.com/vorburger/MariaDB4j/issues/501 - Fix for spaces in data path doesn't work on windows:
+        // Internally Runtime.exec() is being used, which says that an argument such
         // as --name="escaped value" isn't escaped, since there's no leading quote
-        // and it contains a space, so it reescapes it, causing applications to split
+        // and it contains a space, so it re-escapes it, causing applications such as mysqld.exe to split
         // it into multiple pieces, which is why we quote the whole arg (key and value) instead.
-        if ("=".equals(separator)) {
+        // We do this trick only on Windows, because on Linux it breaks the behavior.
+        if ("=".equals(separator) && isWindows()) {
             sb.append(argPart1);
             sb.append(separator);
             sb.append(argPart2);
@@ -256,7 +256,7 @@ public class ManagedProcessBuilder {
     /* package-local... let's keep ch.vorburger.exec's API separate from Apache Commons Exec, so it
      * COULD be replaced */
     CommandLine getCommandLine() {
-        if ((getWorkingDirectory() == null) && commonsExecCommandLine.isFile()) {
+        if (getWorkingDirectory() == null && commonsExecCommandLine.isFile()) {
             File exec = new File(commonsExecCommandLine.getExecutable());
             File dir = exec.getParentFile();
             if (dir == null) {
@@ -278,4 +278,9 @@ public class ManagedProcessBuilder {
         return commonsExecCommandLine.toString();
     }
 
+    // inspired by org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS
+    // without security manager support; assumes System.getProperty() works
+    private boolean isWindows() {
+        return System.getProperty("os.name").startsWith("Windows");
+    }
 }